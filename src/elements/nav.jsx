5039cb3cfc60f2a3ec2aaec0a72a67ac16fbd65c
clemk <clemkfr@gmail.com>
2023-10-03 09:03:45 +0200

    first commit

---
+++ src/elements/nav.jsx
@@ -0,0 +1,17 @@
+import './../style/nav'
+import { NavLink, Link } from "react-router-dom";
+
+
+const Nav =()=>{
+
+    return(
+      <header>
+            <nav>
+                
+
+            </nav>
+
+      </header>
+    )
+}
+export default Nav
